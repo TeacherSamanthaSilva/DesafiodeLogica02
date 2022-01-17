3b5c6d297930596b8ce5e799dad6549b41738e13
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-01-16 23:10:11 -0300

    dois if
    Nesse commit terminei os dois if

---
+++ script.js
@@ -0,0 +1,13 @@
+let idade = 28
+if(idade >=18){
+    console.log("você já é maior de idade tem que trabalhar e pagar suas contas")
+}else{
+    console.log("você não precisa trabalhar, mas pelo menos ajude a lavar a louça")
+}
+
+let humano = true
+if(idade >=18 && humano == true){
+    console.log("Pode criar uma conta no google")
+}else{
+    console.log("Você não pode criar uma conta no google")
+}
